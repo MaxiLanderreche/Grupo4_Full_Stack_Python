079d0854873c2c52e33495380eaea4855b141867
krucho <30963725+krucho@users.noreply.github.com>
2022-10-30 16:07:09 -0300

    Breakpoints al carousel

--- main.js
+++ main.js
@@ -21,26 +21,59 @@ document.getElementById("footer").innerHTML = `<div>
 
 //carousel//
 var swiper = new Swiper(".mySwiper", {
-      slidesPerView: 3,
-      spaceBetween: 0,
-      slidesPerGroup: 3,
-      loop: true,
-      loopFillGroupWithBlank: true,
-      pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-      },
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-    });
+   // Default parameters
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  spaceBetween: 10,
+       
+       loop: true,
+       loopFillGroupWithBlank: true,
+       pagination: {
+         el: ".swiper-pagination",
+         clickable: true,
+        },
+       navigation: {
+         nextEl: ".swiper-button-next",
+         prevEl: ".swiper-button-prev",
+       },
+    
+      breakpoints: {
+        320: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween:  10,
+        },
+       
+        640: {
+          slidesPerView: 1,
+          slidesPerGroup: 1,
+          spaceBetween:  10,
+        },
+        800: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+          spaceBetween:  20,
+        },
+        1200: {
+          slidesPerView: 3,
+          slidesPerGroup: 3,
+          spaceBetween:  20,
+        },
+      }
+
+
+     });
+
+
 //carousel_latestworks//
 
 var swiper = new Swiper(".mySwiperlatestworks", {
   slidesPerView: 1,
   spaceBetween: 0,
   slidesPerGroup: 1,
+  autoplay: {
+        delay: 3000,
+        },
   loop: true,
   loopFillGroupWithBlank: true,
   pagination: {
